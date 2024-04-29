5a68ab7169ff187b27839149c4d8520d40253475
riashad-repliq <riashad.hassan@repliq.dev>
2024-04-29 10:16:47 +0600

    last

--- core/rest/tests/custom_data_generator.py
+++ core/rest/tests/custom_data_generator.py
@@ -15,7 +15,6 @@ import string
 def generate_random_order_no():
     digits = random.randint(100000, 999999)
     letters = ""
-
     for i in range(4):
         char = random.choice(string.ascii_uppercase)
         letters += char
